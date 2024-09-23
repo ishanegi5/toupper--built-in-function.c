5dc20bb26cff000a2949f84a3eb795b007cfacc3
Isha Negi <itsishanegi@gmail.com>
2024-09-23 07:35:49 +0530

    Add files via upload

---
+++ toupper()-built-in-function.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+#include<ctype.h>
+
+void main()
+
+{ char str1[50]="hello, world!";
+  
+char str2[50]; 
+  
+printf("Lower case of string: ");
+  
+puts(str1);
+  
+for(int i=0;i<sizeof(str1);i++)
+  
+{
+      str2[i]=toupper(str1[i]);
+  
+}
+ 
+printf("upper case of string: %s",str2);
+
+}
